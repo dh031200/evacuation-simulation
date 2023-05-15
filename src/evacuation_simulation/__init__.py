2dc206eae7c325076b61146ee8ee9a1dc4d1ddd3
Danny Kim <imbird0312@gmail.com>
2023-05-16 01:44:14 +0900

    processing move

--- src/evacuation_simulation/__init__.py
+++ src/evacuation_simulation/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present U.N. Owen <void@some.where>
 #
 # SPDX-License-Identifier: MIT
-from .core import Agent, Environment, show, wait, destroy
+from .core import Agent, AgentPool, Environment, show, wait, destroy
 
-__all__ = 'Agent', 'Environment', 'show', 'wait', 'destroy'
+__all__ = 'Agent', 'AgentPool', 'Environment', 'show', 'wait', 'destroy'
--- src/evacuation_simulation/core/agent.py
+++ src/evacuation_simulation/core/agent.py
@@ -1,12 +1,63 @@
-# import numpy as np
+from random import random, randint
 
 
 class Agent:
-    def __init__(self, loc: list[int, int]):
+    def __init__(self, floor, loc: list[list[int, int]], direction):
         """
-        :param loc: agent location
+        :param floor: Floor where the agent is located
+        :param loc: Coordinates where the agent is located
+        :param direction: The direction the agent is looking
         """
+        self.floor = floor
         self.location = loc
+        self.direction = direction  # 0:N,  1:E,  2:S,  3:W
+
+        # if is_adult:
+        #     if direction % 2:
+        #         self.location.append([loc[0][0] + 1, loc[0][1]])
+        #     else:
+        #         self.location.append([loc[0][0], loc[0][1] + 1])
 
     def move(self, r, c):
-        self.location = [self.location[0] + r, self.location + c]
+        self.location = [[loc[0] + r, loc[1] + c] for loc in self.location]
+
+
+# class Adult(Agent):
+#     def __init__(self, floor, loc: list[list[int, int]], direction):
+#         super().__init__(floor, loc, direction)
+#         if direction % 2:
+#             self.location.append([loc[0][0] + 1, loc[0][1]])
+#         else:
+#             self.location.append([loc[0][0], loc[0][1] + 1])
+
+
+class AgentPool:
+    def __init__(self, floor, generate_frequency, adult_kids_ratio, check_function):
+        self.pool = []
+        self.floor = [*map(int, floor)]
+        self.generate_frequency = generate_frequency
+        self.adult_kids_ratio = adult_kids_ratio
+        self.check_function = check_function
+
+    def generate(self, floor, points):
+        for point in points:
+            if random() < self.generate_frequency:
+                direction = randint(0, 3)
+                point = [list(point)]
+                if random() < self.adult_kids_ratio:
+                    if direction % 2:
+                        point.append([point[0][0] + 1, point[0][1]])
+                    else:
+                        point.append([point[0][0], point[0][1] + 1])
+                y, x = self.check_function(floor, point)
+                if any([y, x]):
+                    new_points = [[i + y, j + x] for i, j in point]
+                    agent = Agent(floor=floor, loc=new_points, direction=direction)
+                    self.pool.append(agent)
+
+    def move(self, floor):
+        for agent in self.pool:
+            y, x = self.check_function(floor, agent.location)
+            if any([y, x]):
+                agent.move(y,x)
+
